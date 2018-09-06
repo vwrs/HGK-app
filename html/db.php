d698021261858fac1b575b8cc41c057ef4b585a7
GakuInoue <omame.dev.omame@gmail.com>
2018-09-06 10:25:09 +0900

    add db.php

---
+++ html/db.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+
+</head>
+
+<body>
+<meta charset = "UFT-8">
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<h1>フォームデータの送信</h1>
+<form action = "db.php" method = "post">
+<p>
+ID：<input type = "text" name ="id"><br/>
+</p>
+<p>
+緯度：<input type = "text" name ="lat"><br/>
+</p>
+<p>
+経度：<input type = "text" name ="lng"><br/>
+</p>
+<input type = "submit" value ="送信">
+</form>
+<?php
+
+require '../vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+function console_log( $data ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $data ) .')';
+  echo '</script>';
+}
+if(isset($_POST['id'])){
+$id = $_POST['id'];
+$lat = $_POST['lat'];
+$lng = $_POST['lng'];
+
+$sdk = new Aws\Sdk([
+    'region'   => 'ap-northeast-1',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+$tableName = 'hgk-db';
+
+$jsonstr = utf8_encode('
+    {
+        "user_id": ' . $id . ',
+        "timestamp": 111,
+        "jsonform": {
+            "lat": ' . $lat . ',
+            "lng": ' . $lng .',
+	    "job": "student",
+	    "gender": "male"
+        }
+    }
+');
+# echo '<pre>';
+# var_dump(utf8_encode($jsonstr));
+# echo '</pre>';
+# echo '<pre>';
+# var_dump(json_decode(utf8_encode($jsonstr)));
+# echo '</pre>';
+# switch (json_last_error()) {
+#         case JSON_ERROR_NONE:
+#             echo ' - No errors';
+#         break;
+#         case JSON_ERROR_DEPTH:
+#             echo ' - Maximum stack depth exceeded';
+#         break;
+#         case JSON_ERROR_STATE_MISMATCH:
+#             echo ' - Underflow or the modes mismatch';
+#         break;
+#         case JSON_ERROR_CTRL_CHAR:
+#             echo ' - Unexpected control character found';
+#         break;
+#         case JSON_ERROR_SYNTAX:
+#             echo ' - Syntax error, malformed JSON';
+#         break;
+#         case JSON_ERROR_UTF8:
+#             echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+#         break;
+#         default:
+#             echo ' - Unknown error';
+#         break;
+#     }
+
+
+$item = $marshaler->marshalJson($jsonstr);
+
+$params = [
+    'TableName' => 'hgk-db',
+    'Item' => $item
+];
+
+echo '<pre>';
+var_dump($item);
+echo '</pre>';
+
+try {
+    $result = $dynamodb->putItem($params);
+    echo "Added item: $id - $lat - $lng\n";
+
+} catch (DynamoDbException $e) {
+    echo "Unable to add item:\n";
+    echo $e->getMessage() . "\n";
+}
+}
+?>
+</body>
+</html>
