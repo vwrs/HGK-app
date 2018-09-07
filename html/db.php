c57ff1cabb58bf0b5e00d6752fac4cf1ca95612b
Hideaki Kanehara <hideaki.kanehara@gmail.com>
2018-09-07 09:08:41 +0900

    WIP

--- html/db.php
+++ html/db.php
@@ -12,37 +12,18 @@
 <script src="js/amazon-cognito.min.js"></script>
 <script src="js/amazon-cognito-identity.min.js"></script>
 <script src="js/session.js"></script>
-	
+
 <!-- jquery //-->
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
-</head>
-
-<body>
 <meta charset = "UFT-8">
 <title>フォームからデータを受け取る</title>
 </head>
+
+
 <body>
-<h1>フォームデータの送信</h1>
-<form action = "db.php" method = "post">
-<p>
-ID：<input type = "text" name ="id" id="username"><br/>
-</p>
-<p>
-緯度：<input type = "text" name ="lat" value="<?=$_POST['lat']?>"><br/>
-</p>
-<p>
-経度：<input type = "text" name ="lng" value="<?=$_POST['lng']?>"><br/>
-</p>
-<p>
-仕事：<input type = "text" name ="job"><br/>
-</p>
-<p>
-性別：<input type = "text" name ="gender"><br/>
-</p>
-<input type = "submit" value ="送信">
-</form>
+
 <?php
 
 require '../vendor/autoload.php';
@@ -86,36 +67,6 @@ $jsonstr = utf8_encode('
         }
     }
 ');
-# echo '<pre>';
-# var_dump(utf8_encode($jsonstr));
-# echo '</pre>';
-# echo '<pre>';
-# var_dump(json_decode(utf8_encode($jsonstr)));
-# echo '</pre>';
-# switch (json_last_error()) {
-#         case JSON_ERROR_NONE:
-#             echo ' - No errors';
-#         break;
-#         case JSON_ERROR_DEPTH:
-#             echo ' - Maximum stack depth exceeded';
-#         break;
-#         case JSON_ERROR_STATE_MISMATCH:
-#             echo ' - Underflow or the modes mismatch';
-#         break;
-#         case JSON_ERROR_CTRL_CHAR:
-#             echo ' - Unexpected control character found';
-#         break;
-#         case JSON_ERROR_SYNTAX:
-#             echo ' - Syntax error, malformed JSON';
-#         break;
-#         case JSON_ERROR_UTF8:
-#             echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-#         break;
-#         default:
-#             echo ' - Unknown error';
-#         break;
-#     }
-
 
 $item = $marshaler->marshalJson($jsonstr);
 
